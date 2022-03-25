7a2762afb2c4a8578fd26a0137e11813ccbe9e82
hungviet94 <hungtradefx@gmail.com>
2022-03-25 21:31:39 +0700

    done

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".header-toggle");
+const menuHeader = document.querySelector(".header-list");
+const expandClass = "is-expand";
+menuToggle.addEventListener("click", function () {
+  menuHeader.classList.add(expandClass);
+});
+
+window.addEventListener("click", function (e) {
+  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
+    menuHeader.classList.remove(expandClass);
+  }
+});
